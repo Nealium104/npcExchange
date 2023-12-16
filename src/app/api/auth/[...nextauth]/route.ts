7fdcbfbcc356cc46effd4809e3ac5a069cdfb579
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-12-16 08:37:50 -0500

    tabling the db stuff on main. Making a new branch.

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-// import { getProviders } from "next-auth/react";
-// import NextAuth from "next-auth";
-
-// const handler = NextAuth({
-//   // something here
-//   <></>
-// });
-
-// export { handler as GET, handler as POST };
